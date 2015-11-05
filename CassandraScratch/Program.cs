d4704f5a375172c6e46e576e0b5dab79fba294d0
Collin Sauve <collinsauve@gmail.com>
2015-11-05 18:10:47 -0500

    initial work

---
+++ CassandraScratch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cassandra;
+using Nito.AsyncEx;
+
+namespace CassandraScratch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TryConnect();
+            AsyncContext.Run(() => TryConnectAsync());
+        }
+
+        private static Task TryConnectAsync()
+        {
+            TryConnect();
+            return Task.FromResult(1);
+        }
+
+        private static void TryConnect()
+        {
+            try
+            {
+                using (var session = Connect())
+                {
+                    Console.WriteLine("Connected");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+
+        private static ISession Connect()
+        {
+            var builder = Cluster.Builder().AddContactPoints(new List<string> {"127.0.0.1"}).WithDefaultKeyspace("test");
+            var cluster = builder.Build();
+            var connectAndCreateDefaultKeyspaceIfNotExists = cluster.ConnectAndCreateDefaultKeyspaceIfNotExists();
+            return connectAndCreateDefaultKeyspaceIfNotExists;
+        }
+    }
+}
